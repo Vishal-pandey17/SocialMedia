669b93d8cf92317997a3384fc170fbc38a8aba65
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-03-28 14:00:25 +0530

    Setting our directory Structure

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use Express Router..
+app.use('/', require('./routes'));
+// Set up view Engine..
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running Server, ${err}`); // interpolation
