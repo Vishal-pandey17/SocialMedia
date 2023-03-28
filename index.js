da0ba67f58c1b879de03b86a3e49572daedc21d4
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-03-28 15:54:06 +0530

    Setup basic file structure successfully

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+//Extract style and script from layout page
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 // use Express Router..
 app.use('/', require('./routes'));
 // Set up view Engine..
